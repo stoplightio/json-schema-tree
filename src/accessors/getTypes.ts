c16711d1a040334bf1bedc0a9284db2f6398439c
Kayla Chun <58235624+kaylachun@users.noreply.github.com>
2023-08-02 13:53:59 -0700

    fix(gettypes.ts): add null type if nullable is true (#21)

--- src/accessors/getTypes.ts
+++ src/accessors/getTypes.ts
@@ -1,20 +1,36 @@
-import type { SchemaNodeKind } from '../nodes/types';
+import { SchemaNodeKind } from '../nodes/types';
 import type { SchemaFragment } from '../types';
 import { isValidType } from './guards/isValidType';
 import { inferType } from './inferType';
 
 export function getTypes(fragment: SchemaFragment): SchemaNodeKind[] | null {
+  const types: SchemaNodeKind[] = [];
+  let isNullable = false;
+
+  if ('nullable' in fragment) {
+    if (fragment.nullable === true) {
+      isNullable = true;
+    }
+  }
   if ('type' in fragment) {
     if (Array.isArray(fragment.type)) {
-      return fragment.type.filter(isValidType);
+      types.push(...fragment.type.filter(isValidType));
     } else if (isValidType(fragment.type)) {
-      return [fragment.type];
+      types.push(fragment.type);
     }
+    if (isNullable && !types.includes(SchemaNodeKind.Null)) {
+      types.push(SchemaNodeKind.Null);
+    }
+    return types;
   }
 
   const inferredType = inferType(fragment);
   if (inferredType !== null) {
-    return [inferredType];
+    types.push(inferredType);
+    if (isNullable && !types.includes(SchemaNodeKind.Null)) {
+      types.push(SchemaNodeKind.Null);
+    }
+    return types;
   }
 
   return null;
